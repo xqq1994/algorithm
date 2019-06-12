9743614e676fa821e45514d91a5ce6c4f495d1e3
syysummer <syysummer@yeah.net>
2019-06-12 21:31:17 +0800

    add 703

---
+++ src/main/java/com/test/algorithm/queue/KthLargestElementInAStream.java
@@ -0,0 +1,56 @@
+package com.test.algorithm.queue;
+
+import java.util.PriorityQueue;
+
+/**
+ * TODO 703. 数据流中的第K大元素
+ * 设计一个找到数据流中第K大元素的类（class）。注意是排序后的第K大元素，不是第K个不同的元素。
+ *
+ * 你的 KthLargest 类需要一个同时接收整数 k 和整数数组nums 的构造器，它包含数据流中的初始元素。每次调用 KthLargest.add，返回当前数据流中第K大的元素。
+ *
+ * 示例:
+ *
+ * int k = 3;
+ * int[] arr = [4,5,8,2];
+ * KthLargest kthLargest = new KthLargest(3, arr);
+ * kthLargest.add(3);   // returns 4
+ * kthLargest.add(5);   // returns 5
+ * kthLargest.add(10);  // returns 5
+ * kthLargest.add(9);   // returns 8
+ * kthLargest.add(4);   // returns 8
+ * 说明:
+ * 你可以假设 nums 的长度≥ k-1 且k ≥ 1。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/kth-largest-element-in-a-stream
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * **/
+public class KthLargestElementInAStream {
+    class KthLargest {
+        final PriorityQueue<Integer> q;
+        final int k;
+        public KthLargest(int k, int[] nums) {
+            this.k = k;
+            q = new PriorityQueue<>();
+            for (int num : nums) {
+                add(num);
+            }
+        }
+
+        public int add(int val) {
+            if(q.size() < k){
+                q.offer(val);
+            }else if(q.peek() < val){
+                q.poll();
+                q.offer(val);
+            }
+            return q.peek();
+        }
+    }
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
+}
