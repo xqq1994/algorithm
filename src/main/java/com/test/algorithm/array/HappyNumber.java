da156d5090550dbcaf57ea510a4939bbc2552f3c
qiangqiang.xiao <qiangqiang.xiao@591hx.com>
2019-06-20 10:33:04 +0800

    add 202

--- src/main/java/com/test/algorithm/array/HappyNumber.java
+++ src/main/java/com/test/algorithm/array/HappyNumber.java
@@ -76,4 +76,22 @@ public class HappyNumber {
         }
 
     }
+    //推荐用法
+    class Solution2 {
+        public boolean isHappy(int n) {
+            if(n == 1 || n == 7) {
+                return true;
+            }
+            if(n < 10) {
+                return false;
+            }
+            int sum = 0;
+            while(n >= 1) {
+                int d = n % 10;
+                sum += d * d;
+                n /= 10;
+            }
+            return isHappy(sum);
+        }
+    }
 }
