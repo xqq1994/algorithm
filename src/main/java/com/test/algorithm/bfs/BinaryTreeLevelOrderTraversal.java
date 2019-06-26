572490f16bf93a9469be4e006a6edeaccb049a97
syysummer <syysummer@yeah.net>
2019-06-27 00:35:43 +0800

    add 102

---
+++ src/main/java/com/test/algorithm/bfs/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,88 @@
+package com.test.algorithm.bfs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * TODO 102. 二叉树的层次遍历
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ * <p>
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其层次遍历结果：
+ * <p>
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class BinaryTreeLevelOrderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        //BFS
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (root == null) {
+                return res;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> curLevel = new ArrayList<>();
+                for (int i = 0; i < size; i++) {
+                    TreeNode poll = queue.poll();
+                    curLevel.add(poll.val);
+                    if (poll.left != null) queue.add(poll.left);
+                    if (poll.right != null) queue.add(poll.right);
+                }
+                res.add(curLevel);
+            }
+            return res;
+        }
+
+        //DFS
+        public List<List<Integer>> levelOrder1(TreeNode root) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (root == null) {
+                return res;
+            }
+            dfs(root, res, 0);
+            return res;
+        }
+
+        public void dfs(TreeNode root, List<List<Integer>> list, Integer level) {
+            if (root == null) return;
+            if (list.size() < level + 1) {
+                list.add(new ArrayList<>());
+            }
+            List<Integer> integers = list.get(level);
+            integers.add(root.val);
+            dfs(root.left,list,level +1);
+            dfs(root.right,list,level +1);
+        }
+    }
+
+}
