b67fdce2b73ce2e32c537a095a985e7190f66975
syysummer <syysummer@yeah.net>
2019-06-28 23:25:08 +0800

    add 22

--- src/main/java/com/test/algorithm/bfsordfs/GenerateParentheses.java
+++ src/main/java/com/test/algorithm/bfsordfs/GenerateParentheses.java
@@ -25,31 +25,7 @@ import java.util.List;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * **/
 public class GenerateParentheses {
-    public static String stringListToString(List<String> stringList) {
-        StringBuilder sb = new StringBuilder("[");
-        for (String item : stringList) {
-            sb.append(item);
-            sb.append(",");
-        }
-
-        sb.setCharAt(sb.length() - 1, ']');
-        return sb.toString();
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        while ((line = in.readLine()) != null) {
-            int n = Integer.parseInt(line);
-
-            List<String> ret = new Solution().generateParenthesis(n);
-
-            String out = stringListToString(ret);
-
-            System.out.print(out);
-        }
-    }
-    static class Solution {
+    class Solution {
         public List<String> generateParenthesis(int n) {
             List<String> result = new ArrayList<>();
             generateOneByOne("",result,n,n);
