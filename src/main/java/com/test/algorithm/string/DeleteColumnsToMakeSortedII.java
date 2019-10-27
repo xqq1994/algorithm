dac9d323d2748acf2e9f85d58d5e834caba14f44
syysummer <syysummer@yeah.net>
2019-10-27 11:50:35 +0800

    add 955

---
+++ src/main/java/com/test/algorithm/string/DeleteColumnsToMakeSortedII.java
@@ -0,0 +1,76 @@
+package com.test.algorithm.string;
+
+
+/**
+ * TODO 955. 删列造序 II
+ * 给定由 N 个小写字母字符串组成的数组 A，其中每个字符串长度相等。
+ *
+ * 选取一个删除索引序列，对于 A 中的每个字符串，删除对应每个索引处的字符。
+ *
+ * 比如，有 A = ["abcdef", "uvwxyz"]，删除索引序列 {0, 2, 3}，删除后 A 为["bef", "vyz"]。
+ *
+ * 假设，我们选择了一组删除索引 D，那么在执行删除操作之后，最终得到的数组的元素是按 字典序（A[0] <= A[1] <= A[2] ... <= A[A.length - 1]）排列的，然后请你返回 D.length 的最小可能值。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：["ca","bb","ac"]
+ * 输出：1
+ * 解释：
+ * 删除第一列后，A = ["a", "b", "c"]。
+ * 现在 A 中元素是按字典排列的 (即，A[0] <= A[1] <= A[2])。
+ * 我们至少需要进行 1 次删除，因为最初 A 不是按字典序排列的，所以答案是 1。
+ * 示例 2：
+ *
+ * 输入：["xc","yb","za"]
+ * 输出：0
+ * 解释：
+ * A 的列已经是按字典序排列了，所以我们不需要删除任何东西。
+ * 注意 A 的行不需要按字典序排列。
+ * 也就是说，A[0][0] <= A[0][1] <= ... 不一定成立。
+ * 示例 3：
+ *
+ * 输入：["zyx","wvu","tsr"]
+ * 输出：3
+ * 解释：
+ * 我们必须删掉每一列。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= A.length <= 100
+ * 1 <= A[i].length <= 100
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/delete-columns-to-make-sorted-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * **/
+public class DeleteColumnsToMakeSortedII {
+    static class Solution {
+        public int minDeletionSize(String[] A) {
+            int N = A.length;
+            int W = A[0].length();
+            boolean[] cuts = new boolean[N - 1];
+            int ans = 0;
+            f:for (int j = 0; j < W; j++) {
+                for (int i = 0; i < N - 1; i++) {
+                    if(cuts[i] && A[i].charAt(j) > A[i+1].charAt(j)){
+                        ans++;
+                        continue f;
+                    }
+                }
+                for (int i = 0; i < N - 1; i++) {
+                    if(A[i].charAt(j) < A[i+1].charAt(j)){
+                        cuts[i] = true;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().minDeletionSize(new String[]{"xga","xfb","yfa"}));
+    }
+}
